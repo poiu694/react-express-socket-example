4b7dae88407a55b1a1272b953cb8499d8f02d9ef
Lee <43488305+poiu694@users.noreply.github.com>
2022-03-07 16:34:02 +0900

    [Feat] Use global constant, type

---
+++ client/src/util/constant.ts
@@ -0,0 +1,3 @@
+export const CHAT_MESSAGE = 'chat message';
+export const SEND_MESSAGE = 'send message';
+export const NICKNAME = 'nickname';
---
+++ client/src/util/type.ts
@@ -0,0 +1,11 @@
+export type Message = {
+  _id: string;
+  nickname: string;
+  content: string;
+};
+
+export type Item = {
+  _id?: string;
+  nickname: string;
+  content: string;
+};
